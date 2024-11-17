57937ed646f8d7332577dd27649ea090b6ab47e9
sbrown <sbrown@redironlabs.com>
2024-11-16 23:48:25 -0700

    Setup the game scene.

---
+++ Assets/Scripts/SpiceUnit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiceUnit
+{
+    public int brown;
+    public int red;
+    public int green;
+    public int yellow;
+}
